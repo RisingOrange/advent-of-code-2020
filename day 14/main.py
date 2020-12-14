10ae4e2ef78966d82fe7a18785efd8ae8a8336a6
RisingOrange <31575114+RisingOrange@users.noreply.github.com>
2020-12-14 11:30:34 +0100

    Change day 14 implementeation to use zfill

--- day 14/main.py
+++ day 14/main.py
@@ -16,19 +16,19 @@ def execute_lines(lines, write_function):
 
 
 def mask_written_value(memory, address, value, mask):
-    value_bits = list(reversed(bin(value)[2:])) + ['0'] * len(mask)
-    for i, mask_bit in enumerate(reversed(mask)):
+    value_bits = list(bin(value)[2:].zfill(len(mask)))
+    for i, mask_bit in enumerate(mask):
         if mask_bit == '0':
             value_bits[i] = '0'
         elif mask_bit == '1':
             value_bits[i] = '1'
-    masked_value = int(''.join(reversed(value_bits)), 2)
+    masked_value = int(''.join(value_bits), 2)
     memory[address] = masked_value
 
 
 def mask_address(memory, address, value, mask):
-    address_bits = list(reversed(bin(address)[2:])) + ['0'] * len(mask)
-    for i, mask_bit in enumerate(reversed(mask)):
+    address_bits = list(bin(address)[2:].zfill(len(mask)))
+    for i, mask_bit in enumerate(mask):
         if mask_bit == '1':
             address_bits[i] = '1'
         if mask_bit == 'X':
@@ -40,7 +40,7 @@ def mask_address(memory, address, value, mask):
         for i, bit in enumerate(concrete_address_bits):
             if bit == 'X':
                 concrete_address_bits[i] = option_list.pop()
-        concrete_address = int(''.join(reversed(concrete_address_bits)), 2)
+        concrete_address = int(''.join(concrete_address_bits), 2)
         memory[concrete_address] = value
 
 
